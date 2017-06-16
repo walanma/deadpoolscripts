d6c4ab26b11d84e90f35b7a504daae2408bce211
watermelonma <watermelonma@hotmail.com>
2017-06-16 19:54:44 +1200

    updating credentials

--- Extpush.bat
+++ Extpush.bat
@@ -6,13 +6,13 @@ cd C:\dp\deadpooldata
 del /f 1.csv, 2.csv, 3.csv
 git config --global user.email "deadpooldata@gmail.com"
 git config --global user.name "deadpooldata"
+git config --global credential.helper store
 git init
 git remote add origin git@github.com:watermelonma/deadpooldata.git
 git checkout master
 git add .
 git commit -am "Daily update"
 git push -u origin master 
-git config --global credential.helper store
 cd .. 
 rmdir deadpooldata /s /q 
 rmdir deadpoolscripts /s /q
