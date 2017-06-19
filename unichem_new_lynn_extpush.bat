3d783c0dc497aeaa6ecd64c2be0a6cb90ae68ab4
watermelonma <watermelonma@hotmail.com>
2017-06-19 19:01:03 +1200

    fixing \

--- unichem_new_lynn_extpush.bat
+++ unichem_new_lynn_extpush.bat
@@ -1,5 +1,5 @@
 sqlcmd -i "c:\Program Files\dp\deadpoolscripts\unichem_new_lynn.sql" -o "c:\Program Files\dp\deadpooldata\1.csv" -s, -W
-findstr /v /c:"---" "c:\Program Files\dp\deadpooldata\1.csv" > "c:\Program Files\\dp\deadpooldata\2.csv" 
+findstr /v /c:"---" "c:\Program Files\dp\deadpooldata\1.csv" > "c:\Program Files\dp\deadpooldata\2.csv" 
 findstr /v /c:"bottle" "c:\Program Files\dp\deadpooldata\2.csv" > "c:\Program Files\dp\deadpooldata\3.csv" 
 findstr /v /c:"Changed database context to 'ToniqDatabase'." "c:\Program Files\dp\deadpooldata\3.csv" > "c:\Program Files\dp\deadpooldata\unichem_new_lynn.csv" 
 cd "C:\Program Files\dp\deadpooldata"
