478755c83fdc2b0bc1e6b0736b518cb0b9a53b08
watermelonma <watermelonma@hotmail.com>
2017-06-19 19:02:34 +1200

    Updating directory

--- stlukes_extpush.bat
+++ stlukes_extpush.bat
@@ -1,8 +1,8 @@
-sqlcmd -i "c:\Program Files (x86)\dp\deadpoolscripts\stlukes.sql" -o "c:\Program Files (x86)\dp\deadpooldata\1.csv" -s, -W
-findstr /v /c:"---" "c:\Program Files (x86)\dp\deadpooldata\1.csv" > "c:\Program Files (x86)\dp\deadpooldata\2.csv" 
-findstr /v /c:"bottle" "c:\Program Files (x86)\dp\deadpooldata\2.csv" > "c:\Program Files (x86)\dp\deadpooldata\3.csv" 
-findstr /v /c:"Changed database context to 'ToniqDatabase'." "c:\Program Files (x86)\dp\deadpooldata\3.csv" > "c:\Program Files (x86)\dp\deadpooldata\stlukes.csv" 
-cd "C:\Program Files (x86)\dp\deadpooldata"
+sqlcmd -i "c:\dp\deadpoolscripts\stlukes.sql" -o "c:\dp\deadpooldata\1.csv" -s, -W
+findstr /v /c:"---" "c:\dp\deadpooldata\1.csv" > "c:\dp\deadpooldata\2.csv" 
+findstr /v /c:"bottle" "c:\dp\deadpooldata\2.csv" > "c:\dp\deadpooldata\3.csv" 
+findstr /v /c:"Changed database context to 'ToniqDatabase'." "c:\dp\deadpooldata\3.csv" > "c:\dp\deadpooldata\stlukes.csv" 
+cd "C:\dp\deadpooldata"
 del /f 1.csv, 2.csv, 3.csv
 git config --global user.email "deadpooldata@gmail.com"
 git config --global user.name "deadpooldata"
