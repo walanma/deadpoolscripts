38c0f78ee3c3433cb377e7c372616173735de8e7
deadpooldata <deadpooldata@gmail.com>
2017-06-19 14:53:52 +1200

    fix

--- unichem_new_lynn_extpush.bat
+++ unichem_new_lynn_extpush.bat
@@ -1,7 +1,7 @@
-sqlcmd -i "c:\Program Files\dp\deadpoolscripts\unichem_new_lynn.sql" -o "c:\Program Files\dp\dp\deadpooldata\1.csv" -s, -W
-findstr /v /c:"---" "c:\Program Files\dp\deadpooldata\1.csv" > "c:\Program Files\dp\dp\deadpooldata\2.csv" 
-findstr /v /c:"bottle" "c:\Program Files\dp\deadpooldata\2.csv" > "c:\Program Files\dp\dp\deadpooldata\3.csv" 
-findstr /v /c:"Changed database context to 'ToniqDatabase'." "c:\Program Files\dp\dp\deadpooldata\3.csv" > "c:\Program Files\dp\deadpooldata\unichem_new_lynn.csv" 
+sqlcmd -i "c:\Program Files\dp\deadpoolscripts\unichem_new_lynn.sql" -o "c:\Program Files\dp\deadpooldata\1.csv" -s, -W
+findstr /v /c:"---" "c:\Program Files\dp\deadpooldata\1.csv" > "c:\Program Files\\dp\deadpooldata\2.csv" 
+findstr /v /c:"bottle" "c:\Program Files\dp\deadpooldata\2.csv" > "c:\Program Files\dp\deadpooldata\3.csv" 
+findstr /v /c:"Changed database context to 'ToniqDatabase'." "c:\Program Files\dp\deadpooldata\3.csv" > "c:\Program Files\dp\deadpooldata\unichem_new_lynn.csv" 
 cd "C:\Program Files\dp\deadpooldata"
 del /f 1.csv, 2.csv, 3.csv
 git config --global user.email "deadpooldata@gmail.com"
