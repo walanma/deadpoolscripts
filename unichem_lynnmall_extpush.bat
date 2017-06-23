e60dbb06e2c519d88074dea84086dfc6da9c46e2
watermelonma <watermelonma@hotmail.com>
2017-06-23 20:13:35 +1200

    Disabling push for now

--- unichem_lynnmall_extpush.bat
+++ unichem_lynnmall_extpush.bat
@@ -4,15 +4,6 @@ findstr /v /c:"bottle" "c:\dp\deadpooldata\2.csv" > "c:\dp\deadpooldata\3.csv"
 findstr /v /c:"Changed database context to 'ToniqDatabase'." "c:\dp\deadpooldata\3.csv" > "c:\dp\deadpooldata\unichem_lynnmall.csv" 
 cd "C:\dp\deadpooldata"
 del /f 1.csv, 2.csv, 3.csv
-git config --global user.email "deadpooldata@gmail.com"
-git config --global user.name "deadpooldata"
-git config --global credential.helper store
-git init
-git remote add origin git@github.com:watermelonma/deadpooldata.git
-git checkout master
-git add .
-git commit -am "Daily update"
-git push -u origin master 
 cd .. 
 rmdir deadpooldata /s /q 
 rmdir deadpoolscripts /s /q
