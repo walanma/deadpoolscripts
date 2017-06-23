02841de4d2b6216ac082da8731cfd0369024c0ec
watermelonma <watermelonma@hotmail.com>
2017-06-24 11:00:04 +1200

    No push

--- unichem_new_lynn_extpush.bat
+++ unichem_new_lynn_extpush.bat
@@ -4,15 +4,6 @@ findstr /v /c:"bottle" "c:\Program Files\dp\deadpooldata\2.csv" > "c:\Program Fi
 findstr /v /c:"Changed database context to 'ToniqDatabase'." "c:\Program Files\dp\deadpooldata\3.csv" > "c:\Program Files\dp\deadpooldata\unichem_new_lynn.csv" 
 cd "C:\Program Files\dp\deadpooldata"
 del /f 1.csv, 2.csv, 3.csv
-git config --global user.email "deadpooldata@gmail.com"
-git config --global user.name "deadpooldata"
-git config --global credential.helper store
-git init
-git remote add origin git@github.com:watermelonma/deadpooldata.git
-git checkout master
-git add .
-git commit -am "Daily update"
-git push -u origin master 
 cd .. 
 rmdir deadpooldata /s /q 
 rmdir deadpoolscripts /s /q
